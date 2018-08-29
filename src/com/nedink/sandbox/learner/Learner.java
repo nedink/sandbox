2ee159a27f703c37fee818a81ecc2dfed4adead0
David Bryant <nedink@gmail.com>
2018-08-29 17:25:31 -0400

    feat(learner): init

---
+++ src/com/nedink/sandbox/learner/Learner.java
@@ -0,0 +1,12 @@
+package com.nedink.sandbox.learner;
+
+import java.util.Random;
+
+public class Learner {
+
+    float size;
+
+    Learner() {
+        size = new Random().nextFloat();
+    }
+}
---
+++ src/com/nedink/sandbox/learner/Main.java
@@ -0,0 +1,15 @@
+package com.nedink.sandbox.learner;
+
+public class Main {
+
+    static final int NUMBER = 10;
+
+    public static void main(String[] args) {
+
+
+
+        for (int i = 0; i < NUMBER; i++) {
+            System.out.println(i);
+        }
+    }
+}
